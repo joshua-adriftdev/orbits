166d53cba1d78cd3d993026bb81b0ddf86e065c4
ItsAdrift <joshua@adrift.dev>
2024-04-21 22:22:03 +1000

    Tooltip Modifications + Introduction Popup

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,21 +5,49 @@ import {
     IconButton,
     Typography,
   } from "@material-tailwind/react";
-  import React, { useState } from "react";
+  import React, { useEffect, useState } from "react";
   
-  export function Introduction() {
-    const [open, setOpen] = useState<boolean>(true);
+  type IntroductionProps = {
+    isOpen: boolean;
+    setIsOpen: (open: boolean) => void;
+  };
+  
+  const Introduction: React.FC<IntroductionProps> = ({ isOpen, setIsOpen }) => {
+    const [open, setOpen] = useState<boolean>(isOpen);
+    const [closing, setClosing] = useState<boolean>(false);
+  
+    useEffect(() => {
+      setOpen(isOpen);
+    }, [isOpen]);
   
     const handleClose = () => {
-      setOpen(false);
+      setClosing(true);
+      setTimeout(() => {
+        setOpen(false);
+        setIsOpen(false);
+        setClosing(false); // Reset closing state after animation
+      }, 500); // Adjust duration to match animation duration
+    };
+  
+    const handleReopen = () => {
+      setOpen(true);
+      setClosing(false);
     };
   
     return (
-      <section className={`transition-opacity duration-500 ${open ? "" : "opacity-0 pointer-events-none"}`}>
+      <section
+        className={`transition-opacity duration-500 ${
+          closing ? "opacity-0 pointer-events-none" : ""
+        }`}
+      >
         <div className="w-full px-4">
           <div className="grid h-screen place-items-center">
             <div className="fixed inset-0 bg-black opacity-30 z-20"></div>
-            <Card className="max-w-xl lg:max-w-3xl relative z-20">
+            <Card
+              className={`max-w-xl relative z-20 ${
+                closing ? "" : "pointer-events-auto"
+              }`}
+            >
               <CardBody>
                 <div className="flex w-full justify-end">
                   <IconButton variant="text" onClick={handleClose}>
@@ -34,26 +62,26 @@ import {
                 </div>
                 <div className="text-center px-6">
                   <Typography
-                    className="mb-6 mt-[-1.5rem] text-content"
+                    className="mb-6 mt-[-2rem] text-content"
                     variant="h4"
                   >
                     How To Play
                   </Typography>
-                  <div className="flex flex-col gap-5 text-contentSecondary font-normal text-[20px]">
-                    <Typography className="text-[14px] lg:text-[16px]">
+                  <div className="flex flex-col gap-3 text-contentSecondary font-normal text-[20px]">
+                    <Typography className="text-[16px]">
                       Welcome to Orbits, the circular word puzzle that challenges
                       your associations!
                     </Typography>
-                    <Typography className="text-[14px] lg:text-[16px]">
+                    <Typography className="text-[16px]">
                       Each word connects to another through a theme, forming a
                       complete loop. The challenge is to find the sequence.
                     </Typography>
-                    <Typography className="text-[14px] lg:text-[16px]">
+                    <Typography className="text-[16px]">
                       You can start with any word, but choose wisely— each word
                       leads to the next, and the final word will connect back to
                       your first choice.
                     </Typography>
-                    <Typography className="text-[14px] lg:text-[16px]">
+                    <Typography className="text-[16px]">
                       If you make an incorrect guess, don't worry— you'll get a
                       chance to try again.
                     </Typography>
@@ -61,9 +89,9 @@ import {
                   <Button
                     size="lg"
                     className="mt-8 px-12 bg-primary"
-                    onClick={handleClose}
+                    onClick={open ? handleClose : handleReopen}
                   >
-                    Let's Play!
+                    {"Let's Play!"}
                   </Button>
                 </div>
               </CardBody>
@@ -72,7 +100,7 @@ import {
         </div>
       </section>
     );
-  }
+  };
   
   export default Introduction;
   
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import Head from "next/head";
 import WordBankTooltip from "@/components/tooltip/WordBankTooltip";
 import DisplayTooltip from "@/components/tooltip/DisplayTooltip";
 import Introduction from "@/components/Introduction";
+import { hasCookie } from "cookies-next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +37,8 @@ export default function Home() {
 
   const [data, setData] = useState<Orbit | null>(null);
   const [loading, setLoading] = useState(true);
+
+  const [showIntroduction, setShowIntroduction] = useState<boolean>(!hasCookie("user"))
 
   useEffect(() => {
     const fetchData = async () => {
@@ -81,7 +84,6 @@ export default function Home() {
     setMistakes(mistakes+1);
 
     /* Fail
-
     // Disable all buttons
     setForceDisable(true);
 
@@ -116,14 +118,19 @@ export default function Home() {
   const displayDate:string = date.toLocaleDateString('default', { month: 'long'}) + " " + date.getDate() + ", " + date.getFullYear()
 
 
+  const handleChangeIntroductionState = (state: boolean) => {
+    console.log("Setting to: " + state);
+    setShowIntroduction(state);
+  }
+
   return (
     <div>
       <Head>
         <title>Orbits</title>
       </Head>
       <main className={`flex flex-col items-center justify-between ${inter.className}`}>
-        <div className="fixed z-30">
-          <Introduction/>
+        <div className={showIntroduction ? "fixed z-30" : "hidden"}>
+          <Introduction isOpen={showIntroduction} setIsOpen={handleChangeIntroductionState}/>
         </div>
         <Image src={banner} alt="alt" width={240} height={75} className="mt-4 lg:hidden"/>
         <Image src={banner} alt="alt" width={300} height={0} className="mt-4 hidden lg:block"/>
@@ -132,11 +139,12 @@ export default function Home() {
         <div className="w-full flex flex-col items-center justify-center">
           <div className="font-light text-[16px] lg:text-[24px]">{displayDate}</div>
           <div className="font-medium text-[24px] lg:text-[30px]">Theme: <span className="font-normal">{theme}</span></div>
+          <div className="text-content text-[18px] font-light">Mistakes: {mistakes}</div>
         </div>
 
         <div className="w-full flex justify-center items-center mt-4">
           <div className="grid grid-cols-3 w-[290px] lg:w-[670px]">
-            <div className="flex justify-end items-end col-start-3"><DisplayTooltip/></div>
+            <div className="flex justify-end items-end col-start-3"><DisplayTooltip isOpen={showIntroduction} setIsOpen={handleChangeIntroductionState}/></div>
           </div>
         </div>
 
@@ -149,11 +157,10 @@ export default function Home() {
             <div className="justify-self-center col-start-2">
               <div className="flex flex-col items-center">
                 <div className="text-content text-[18.5px] lg:text-[24px] font-medium lg:font-normal">Word Bank</div>
-                <div className="text-content text-[15px] font-light">Mistakes: {mistakes}</div>
+                
               </div>
             </div>
-
-            <div className="flex justify-end items-end"><WordBankTooltip/></div>
+            <div className="flex justify-end items-end "><WordBankTooltip/></div>
           </div>
         </div>
 
